678cce2a167e42a19de2020c5b99b9b07e508c2a
caseychin <caseyemchin@gmail.com>
2025-11-19 15:21:38 +0900

    requirements.txt + load_data functions

--- streamlit/overview.py
+++ streamlit/overview.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+from utils import *
 
 st.write("# Overview Page")
 
 st.write("Welcome to the Overview page of the GM Credit Fund Risk Model application.")
+
+df = load_data('./data/companies.csv')
+clean_df = load_data('./processed_data/companies_cleaned_data.csv')
+
+st.write(df)
+st.write(clean_df)
 
